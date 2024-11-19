c78c9e07cb3663205915d43cb9490dbdc0ac75be
rudeuns <rudeuns@gmail.com>
2024-11-19 19:02:04 +0900

    chore: Add initial project files

---
+++ src/main/java/org/mentalk/MentalkApplication.java
@@ -0,0 +1,13 @@
+package org.mentalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalkApplication.class, args);
+    }
+
+}
