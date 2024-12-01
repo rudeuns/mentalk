7761e88ebeb5777fdfc94b717358adca90fa49d7
rudeuns <rudeuns@gmail.com>
2024-12-02 00:32:46 +0900

    test: 테스트 모의 사용자 어노테이션 수정

--- src/test/java/org/mentalk/annotation/WithCustomMockUser.java
+++ src/test/java/org/mentalk/annotation/WithCustomMockUser.java
@@ -2,8 +2,7 @@ package org.mentalk.annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import org.mentalk.enums.LoginProvider;
-import org.mentalk.enums.Role;
+import org.mentalk.common.enums.Role;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -13,6 +12,4 @@ public @interface WithCustomMockUser {
     long id() default 1;
 
     Role role() default Role.USER;
-
-    LoginProvider provider() default LoginProvider.OURS;
 }
--- src/test/java/org/mentalk/annotation/WithCustomSecurityContextFactory.java
+++ src/test/java/org/mentalk/annotation/WithCustomSecurityContextFactory.java
@@ -1,9 +1,8 @@
 package org.mentalk.annotation;
 
 import java.lang.annotation.Annotation;
-import org.mentalk.enums.LoginProvider;
-import org.mentalk.enums.Role;
-import org.mentalk.security.PrincipalDetails;
+import org.mentalk.common.enums.Role;
+import org.mentalk.common.security.PrincipalDetails;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -17,19 +16,16 @@ public class WithCustomSecurityContextFactory
     public SecurityContext createSecurityContext(Annotation annotation) {
         long id = 1;
         Role role = Role.USER;
-        LoginProvider provider = LoginProvider.OURS;
 
         if (annotation instanceof WithCustomMockUser customMockUser) {
             id = customMockUser.id();
             role = customMockUser.role();
-            provider = customMockUser.provider();
         } else if (annotation instanceof WithCustomMockMentor customMockMentor) {
             id = customMockMentor.id();
             role = customMockMentor.role();
-            provider = customMockMentor.provider();
         }
 
-        PrincipalDetails principal = new PrincipalDetails(id, role, provider);
+        PrincipalDetails principal = new PrincipalDetails(id, role);
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(principal, null,
                                                                                 principal.getAuthorities());
