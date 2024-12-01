da1c4cad4de85184a6dbc83be41427a363187546
rudeuns <rudeuns@gmail.com>
2024-12-02 00:18:14 +0900

    feat: 보안 설정 및 인증 추가

---
+++ src/main/java/org/mentalk/common/security/PrincipalDetails.java
@@ -0,0 +1,26 @@
+package org.mentalk.common.security;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.mentalk.common.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public record PrincipalDetails(Long id, Role role) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+}
